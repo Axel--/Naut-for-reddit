f4ae140c26d66e333b00443270d1f6ecbb615546
derw <misc.dmacht777@gmail.com>
2019-02-16 01:56:07 -0700

    Add automatic upload functionality

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,14 @@
 var pkg = require('./package.json');
 
 var gulp = require('gulp'),
+    fs = require('fs'),
     sass = require('gulp-sass'),
     cleanCSS = require('gulp-clean-css'),
     replace = require('gulp-replace'),
     header = require('gulp-header'),
     footer = require('gulp-footer'),
-    gutil = require('gulp-util');
+    gutil = require('gulp-util'),
+    snoowrap = require('snoowrap');
 
 var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
     t = new Date(),
@@ -52,4 +54,42 @@ gulp.task('compile', () => {
 // Watch task
 gulp.task('watch', () => {
     gulp.watch('src/**/*', gulp.series('compile'));
+});
+
+// Upload to reddit
+gulp.task('reddit-upload', () => {
+    return new Promise(function(resolve, reject) {
+        try {
+            let reddit = require('./reddit.json');
+
+            try {
+                const r = new snoowrap({
+                    userAgent: `${pkg.name} ${pkg.version}`,
+                    clientId: reddit.client_id,
+                    clientSecret: reddit.client_secret,
+                    refreshToken: reddit.refresh_token
+                });
+
+                r.getSubreddit(reddit.subreddit).updateStylesheet({css: fs.readFileSync("./stylesheet.css", "utf-8"), reason: `${pkg.name} ${pkg.version}`});
+                gutil.log(`Successfully uploaded to ${reddit.subreddit}`);
+
+                resolve();
+            }
+
+            catch(e) {
+                gutil.log('Error uplaoding to subreddit: ', e);
+                reject();
+            }
+        }
+
+        catch(e) {
+            gutil.log('Error loading reddit config: ', e);
+            reject();
+        }
+    });
+});
+
+// Watch and upload to reddit
+gulp.task('watch-upload', () => {
+    gulp.watch('src/**/*', gulp.series('compile', 'reddit-upload'));
 });
